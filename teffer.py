12008e48a506821584fbf065b3c79cd1ab1ef4d0
bddicken <bddicken@gmail.com>
2018-08-22 13:45:31 -0700

    update

--- teffer.py
+++ teffer.py
@@ -156,7 +156,7 @@ def put_strings_side_by_side(a, b):
             result += '      |'
             result += not_matching_begin + left + not_matching_end + '|' + right + '\n'
             diff_i = find_first_difference_index(left, right)
-            carat =  ((' ' * (diff_i-1)) + '^').ljust(widest) + '|\n'
+            carat =  ((' ' * (diff_i)) + '^').ljust(widest) + '|\n'
             result += '>>>>>>|' + carat
         else: 
             result += '      |'
